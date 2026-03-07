8292fa1d33fbd70d67f3259527066008ee711108
magaliek <magrobkal@gmail.com>
2026-03-07 07:40:28 +0100

    finished task 2 with caveat

---
+++ Assets/contacts scripts/doneScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace contacts_scripts
+{
+    public class DoneScript : MonoBehaviour
+    {
+        [SerializeField] private NoteborderScript[] validZones;
+        [SerializeField] private GameObject panel;
+
+        private bool _anyEmpty;
+        
+        private bool IsRed()
+        {
+            bool anyRed = false;
+            _anyEmpty = false;
+            foreach (var zone in validZones)
+            {
+                Outline outline = zone.GetComponent<Outline>();
+                if (outline.enabled) anyRed = true;
+                if (zone.isEmpty) _anyEmpty = true;
+            }
+            return anyRed;
+        }
+
+        public void OnDoneClicked()
+        {
+            if (IsRed() || _anyEmpty)
+            {
+                GetComponent<AudioSource>().Play();
+                return;
+            };
+            _anyEmpty = false;
+            panel.SetActive(false);
+        }
+    }
+}
---
+++ Assets/packing scripts/doneScriptP.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace packing_scripts
+{
+    public class DoneScriptP : MonoBehaviour
+    {
+        [SerializeField] private GameObject panel;
+
+        public void OnDoneClicked()
+        {
+            panel.SetActive(false);
+        }
+    }
+}
