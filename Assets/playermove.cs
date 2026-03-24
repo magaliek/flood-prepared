1a6b9b578656ec1c48cd4e0e90947aca58a4d010
676660 <142893444+676660@users.noreply.github.com>
2026-03-24 14:56:40 +0100

    Updating the character Movment

--- Assets/playermove.cs
+++ Assets/playermove.cs
@@ -3,24 +3,39 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed = 5f;
-    private Rigidbody2D rb;
-    private Vector2 moveInput;
+     private float speed = 4f;
+     private Rigidbody2D rb;
+    private Vector2 movementInput;
+    private Animator animator;
+
 
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
         //Enable Collider here
     }
 
     void FixedUpdate()
     {
-        rb.linearVelocity = moveInput * moveSpeed;   // 2D standard
+        rb.linearVelocity = movementInput * speed;   // 2D standard
         //Check if collider is enabled here every frame 
     }
 
-    public void Move(InputAction.CallbackContext context)
+    public void Move(InputAction.CallbackContext c)
     {
-        moveInput = context.ReadValue<Vector2>();
+        animator.SetBool("playerWalking", true);
+
+        if (c.canceled)
+        {
+            animator.SetBool("playerWalking", false);
+            animator.SetFloat("X", movementInput.x);
+            animator.SetFloat("Y", movementInput.y);
+
+        }
+        movementInput = c.ReadValue<Vector2>();
+        animator.SetFloat("inputx", movementInput.x);
+        animator.SetFloat("inputy", movementInput.y);
+
     }
 }
