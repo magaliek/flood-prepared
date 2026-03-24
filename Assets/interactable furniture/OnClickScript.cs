856c0be0443a744ae1b19e251fb6cd764b533466
magaliek <magrobkal@gmail.com>
2026-03-24 22:49:24 +0100

    connected the tasks

---
+++ Assets/interactable furniture/OnClickScript.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+namespace interactable_furniture
+{
+    public class OnClickScript : MonoBehaviour
+    {
+        [SerializeField] private GameObject panel;
+        private bool _playerNearby = false;
+
+        void OnCollisionEnter2D(Collision2D col)
+        {
+            if (col.gameObject.CompareTag("Player"))
+                _playerNearby = true;
+        }
+
+        void OnCollisionExit2D(Collision2D col)
+        {
+            if (col.gameObject.CompareTag("Player"))
+                _playerNearby = false;
+        }
+
+        void Update()
+        {
+            if (_playerNearby && Input.GetKeyDown(KeyCode.Return))
+                panel.SetActive(true);
+        }
+    }
+}
