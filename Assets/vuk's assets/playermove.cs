772e0c06e89074faf1bb82ae19ec1fc26f014a51
678146 <678146@stud.hvl.no>
2026-04-09 16:54:27 +0200

    Backup før merge/testing

--- Assets/vuk's assets/playermove.cs
+++ Assets/vuk's assets/playermove.cs
@@ -3,39 +3,24 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-     private float speed = 4f;
-     private Rigidbody2D rb;
-    private Vector2 movementInput;
-    private Animator animator;
-
+    [SerializeField] private float moveSpeed = 5f;
+    private Rigidbody2D rb;
+    private Vector2 moveInput;
 
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
-        animator = GetComponent<Animator>();
         //Enable Collider here
     }
 
     void FixedUpdate()
     {
-        rb.linearVelocity = movementInput * speed;   // 2D standard
+        rb.linearVelocity = moveInput * moveSpeed;   // 2D standard
         //Check if collider is enabled here every frame 
     }
 
-    public void Move(InputAction.CallbackContext c)
+    public void Move(InputAction.CallbackContext context)
     {
-        animator.SetBool("playerWalking", true);
-
-        if (c.canceled)
-        {
-            animator.SetBool("playerWalking", false);
-            animator.SetFloat("X", movementInput.x);
-            animator.SetFloat("Y", movementInput.y);
-
-        }
-        movementInput = c.ReadValue<Vector2>();
-        animator.SetFloat("inputx", movementInput.x);
-        animator.SetFloat("inputy", movementInput.y);
-
+        moveInput = context.ReadValue<Vector2>();
     }
 }
