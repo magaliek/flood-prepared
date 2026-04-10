c407503f8ade9b44ec3a1cf6df26845c9a3ab9a8
676660 <142893444+676660@users.noreply.github.com>
2026-04-10 16:46:01 +0200

    menuchanges

---
+++ Assets/accountScript.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+
+public class accountScript : MonoBehaviour
+
+{
+    public TMP_InputField usernameInput;
+    public TMP_InputField passwordInput;
+
+    public GameObject loginPanel;
+    public GameObject registerPanel;
+
+    private string currentUsername;
+    private string currentPassword;
+
+    // Called when Play button is pressed
+    public void OnPlayPressed()
+    {
+        currentUsername = usernameInput.text;
+        currentPassword = passwordInput.text;
+
+        string savedUser = PlayerPrefs.GetString("username", "");
+        string savedPass = PlayerPrefs.GetString("password", "");
+
+        if (currentUsername == savedUser && currentPassword == savedPass)
+        {
+            LoadGame();
+        }
+        else
+        {
+            // Show register panel
+            loginPanel.SetActive(false);
+            registerPanel.SetActive(true);
+        }
+    }
+
+    // YES button
+    public void OnYesPressed()
+    {
+        // Save new account
+        PlayerPrefs.SetString("username", currentUsername);
+        PlayerPrefs.SetString("password", currentPassword);
+        PlayerPrefs.Save();
+
+        LoadGame();
+    }
+
+    // NO button
+    public void OnNoPressed()
+    {
+        registerPanel.SetActive(false);
+        loginPanel.SetActive(true);
+    }
+
+    void LoadGame()
+    {
+        SceneManager.LoadScene("GameScene"); // change to your scene name
+    }
+}
