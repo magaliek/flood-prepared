47969209415f739dc4fd9d77fcd888e26efb7019
magaliek <magrobkal@gmail.com>
2026-04-12 00:04:25 +0200

    set up supabase client and config

---
+++ Assets/maggies_awesome_score_system/SupaConfig.cs
@@ -0,0 +1,8 @@
+namespace maggies_awesome_score_system {
+
+    public static class SupaConfig
+    {
+        public const string Url = "https://lxohygkeriztynfbotxn.supabase.co";
+        public const string AnonKey = "sb_publishable_JQUIBcZpC_MzeUto1xVvIg_hssaxj4b";
+    }
+}
