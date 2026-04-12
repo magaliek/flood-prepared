f28b15c58c4a7d3fedda8156aa195a9c64a9772b
magaliek <magrobkal@gmail.com>
2026-04-12 03:01:45 +0200

    setup login w/ logic

--- Assets/MenuScripts/MakeAccountScript.cs
+++ Assets/MenuScripts/MakeAccountScript.cs
@@ -1,23 +1,26 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace MenuScripts
 {
     public class MakeAccountScript : MonoBehaviour
     {
+        [SerializeField] private GameObject makeAccPanel;
         // Start is called once before the first execution of Update after the MonoBehaviour is created
         void Start()
         {
-            this.gameObject.SetActive(false);
+            makeAccPanel.SetActive(false);
         }
 
         public void OnNoClick()
         {
-            this.gameObject.SetActive(false);
+            makeAccPanel.SetActive(false);
         }
 
         public void OnYesClick()
         {
-            this.gameObject.SetActive(false);
+            SceneManager.LoadScene("Hallway");
+            makeAccPanel.SetActive(false);
             //TODO: make account
         }
     }
